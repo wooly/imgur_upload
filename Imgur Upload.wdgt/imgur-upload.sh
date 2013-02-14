6819577360b32521ab2d893e30cd40e7949e3425
Steve Bell <stephen.david.bell@googlemail.com>
2013-02-14 14:04:29 +0000

    Correctly extract the URL from the curl response. Closes #1

--- Imgur Upload.wdgt/imgur-upload.sh
+++ Imgur Upload.wdgt/imgur-upload.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo "Uploading! $1"
-image=`curl -s -F "image=@$1" -F "key=58584eba731a360f7d0b78c30e5a8d5f" http://imgur.com/api/upload.xml | grep -o http\:\/\/i\.imgur\.com\/.....[.]...`
+image=`curl -s -F "image=@/Users/steve/Desktop/test.jpg" -F "key=58584eba731a360f7d0b78c30e5a8d5f" http://imgur.com/api/upload.xml | tr -d '\n' | sed -e 's/.*<original\_image>\(.*\)<\/original\_image>.*/\1/'`
 /bin/echo -n $image | pbcopy
